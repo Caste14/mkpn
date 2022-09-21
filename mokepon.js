db11404ae48a14e7f0f547b853ba76f0b5303e0d
root <root@DESKTOP-7D0RJM7.localdomain>
2022-09-20 22:41:22 -0300

    todays advance

--- mokepon.js
+++ mokepon.js
@@ -1,31 +1,56 @@
 function seleccionarMascotaJugador()
 {
-    let inputHd = document.getElementById('Hipodoge')
-    let inputCpp = document.getElementById('Capipepo')
-    let inputRg = document.getElementById('Ratigueya')
-
+    let inputHd = document.getElementById('Hipodoge');
+    let inputCpp = document.getElementById('Capipepo');
+    let inputRg = document.getElementById('Ratigueya');
+    let spanMascotaJugador = document.getElementById('mascota-jugador');
+    let spanVidaJugador = document.getElementById('vida-jugador');
     switch (ckecked = true) {
         case inputHd.checked:
-            alert('Elegiste a Hipodoge🐺')
+            spanMascotaJugador.innerHTML = 'Hipodoge'
+            spanVidaJugador.innerHTML = 5
             break;
         case inputCpp.checked:
-            alert('Elegiste a Capipepo🐻')
+            spanMascotaJugador.innerHTML = 'Capipepo'
+            spanVidaJugador.innerHTML = 3
             break;
         case inputRg.checked:
-            alert('Elegiste a Ratigueya🐭')
+            spanMascotaJugador.innerHTML = 'Ratigueya'
+            spanVidaJugador.innerHTML = 2
             break;
         default:
             alert('Parece que no elegiste a ninguna mascota 🤷‍♂️, por favor, ¡elije una!')
             break;
     }
+    seleccionarMascotaEnemigo();
 }
 
 
 function iniciarJuego()
 {
     let botonMascotaJugador = document.getElementById("boton-mascota");
-botonMascotaJugador.addEventListener("click", seleccionarMascotaJugador)
+botonMascotaJugador.addEventListener("click", seleccionarMascotaJugador);
 
 }
 
 window.addEventListener("load", iniciarJuego);
+
+function seleccionarMascotaEnemigo()
+{
+    let ataqueAleatorio = aleatorio(1,3);
+    let spanMascotaEnemigo = document.getElementById('mascota-enemigo')
+    if (ataqueAleatorio === 1)
+    {
+        spanMascotaEnemigo.innerHTML = 'Hipodoge'
+    } else if (ataqueAleatorio === 2)
+    {
+        spanMascotaEnemigo.innerHTML = 'Capipepo'
+    } else (ataqueAleatorio === 3)
+    {
+        spanMascotaEnemigo.innerHTML = 'Ratigueya'
+    }
+}
+function aleatorio(min, max)
+{
+    return Math.floor(Math.random()*(max - min + 1) + min)
+}
